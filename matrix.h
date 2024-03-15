881f53ccad5f17dd59e78f4a71c418bdb62ad644
A1075 <ariffaishac31@gmail.com>
2024-03-15 22:29:34 +0700

    feat(matrices): Include all module in matrix.c to header file

--- matrix.h
+++ matrix.h
@@ -21,4 +21,23 @@ typedef struct {
   float *value;
 } Matrix;
 
+void is_dim_valid(int nrow, int ncol);
+void is_success_allocate(Matrix *matrix);
+void is_row_col_match(int ncol, int nrow);
+void is_shape_match(Matrix *matrix1, Matrix *matrix2);
+Matrix init(int nrow, int ncol);
+void set_entries(float value, Matrix *matrix);
+Matrix zeros(int nrow, int ncol);
+Matrix ones(int nrow, int ncol);
+double generate_random_normal(void);
+Matrix randn(int nrow, int ncol);
+Matrix transpose(Matrix *matrix);
+Matrix multiplication(Matrix *matrix1, Matrix *matrix2);
+Matrix addition(Matrix *matrix1, Matrix *matrix2);
+Matrix substract(Matrix *matrix1, Matrix *matrix2);
+Matrix scalar_mul(double k, Matrix *matrix1);
+double mean(Matrix *matrix);
+double variance(Matrix *matrix);
+void print_matrix(Matrix *matrix);
+
 #endif
